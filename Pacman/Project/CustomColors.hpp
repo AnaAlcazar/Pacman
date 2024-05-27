b48f70a06daa8b8edd984e6e3093c6db17bc1ade
SrtaIko <analcazarc@gmail.com>
2024-05-27 09:46:31 +0200

    Update

--- Pacman/Project/CustomColors.hpp
+++
@@ -1,8 +0,0 @@
-#define MAP_BLUE {36, 36, 252, 255}
-
-#define TEXT_RED {255, 0, 0, 255}
-#define TEXT_PINK {255, 183, 255, 255}
-#define TEXT_BLUE {0, 255, 255, 255}
-#define TEXT_ORANGE {255, 183, 81, 255}
-#define TEXT_OCHER {255, 187, 178, 255}
-#define TEXT_YELLOW {255, 255, 0, 255}
